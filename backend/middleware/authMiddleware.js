1d43318fd11f3b55e3b80b8c2c07509d02c1b584
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2025-11-18 02:35:45 -0600

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,3 @@
-// backend/middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 
@@ -8,16 +7,15 @@ exports.protect = async (req, res, next) => {
     // 1. Revisar si el token viene en los 'headers' de la petición
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         try {
-            // 2. Obtener el token (quitando la palabra 'Bearer')
+            // 2. Obtener el token
             token = req.headers.authorization.split(' ')[1];
 
             // 3. Verificar el token
-            // (Asegúrate de usar la misma clave secreta que en authController)
-            const JWT_SECRET = 'miclavesecreta123'; 
+            const JWT_SECRET = ''; 
             const decoded = jwt.verify(token, JWT_SECRET);
 
             // 4. Obtener el usuario del token y adjuntarlo a 'req'
-            // (Esto nos permite saber QUIÉN está haciendo la petición)
+            // Quién está haciendo la petición
             req.user = await User.findById(decoded.id).select('-password');
 
             next(); // ¡Pase autorizado! Sigue al siguiente controlador.
