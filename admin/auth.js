9f5e5d7b5177f188a410d4d22a99a39db779585e
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2025-11-18 01:33:02 -0600

    Update auth.js

--- admin/auth.js
+++ admin/auth.js
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const password = passwordInput.value;
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://upcear-cms.onrender.com/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
